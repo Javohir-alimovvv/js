b21e978d8a76d51ef7ebf5e27abcb13673080e1e
javohir <ajavohir969@gmail.com>
2024-01-31 09:41:40 +0500

    js

--- assets/js/add.js
+++ assets/js/add.js
@@ -9,3 +9,38 @@ prompt("Oilada nechta a'zosizlar")
 prompt("Yashash manzilingizni kiriting")
 prompt("Telefon raqamingizni kiriting")
 confirm("Saytimizga obuna bulganingiz uchun rahmat siz bilan bog'lanamiz")
+
+const num1 = 1234;
+const num3 = 1234;
+const num4 = 1234;
+const num5 = 1234;
+const num6 = 1234;
+const num7 = 1234;
+const num8 = 1234;
+const num9 = 1234;
+const num10 = 1234;
+
+let card1 = 456;
+let card2 = 456;
+let card3 = 456;
+let card4 = 456;
+let card5 = 456;
+let card6 = 456;
+let card7 = 456;
+let card8 = 456;
+let card9 = 456;
+let card10 = 456;
+
+var group1 = 9898;
+var group2 = 9898;
+var group3 = 9898;
+var group4 = 9898;
+var group5 = 9898;
+var group6 = 9898;
+var group7 = 9898;
+var group8 = 9898;
+var group9 = 9898;
+var group10 = 9898;
+
+
+
